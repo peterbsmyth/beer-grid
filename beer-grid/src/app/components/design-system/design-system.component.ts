989a794ac03a11d37cef58deae58eb0f21b5194f
James Patrick Gibson <james@jamespatrickgibson.com>
2019-10-14 17:05:05 -0400

    feat: Added style guide component for visuals during architecture setup

---
+++ beer-grid/src/app/components/design-system/design-system.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-design-system',
+  templateUrl: './design-system.component.html',
+  styleUrls: ['./design-system.component.scss']
+})
+export class DesignSystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
